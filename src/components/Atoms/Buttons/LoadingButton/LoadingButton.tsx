47d812ea251bafa851d7c1a155a9f70c0b737f28
zero86 <katanazero86@gmail.com>
2024-02-04 21:13:42 +0900

    - CSS 오타 수정 및 테스트

--- src/components/Atoms/Buttons/LoadingButton/LoadingButton.tsx
+++ src/components/Atoms/Buttons/LoadingButton/LoadingButton.tsx
@@ -2,7 +2,7 @@ import {cva} from "class-variance-authority";
 import {cnUtil} from "@/utils/cnUtil";
 
 
-const loadingButtonVariants = cva(`text-white bg-indigo-700 hover:bg-indigo-800 fcus:outline-none font-medium rounded text-sm px-5 py-2.5 text-center mr-2 inline-flex items-center justify-center`, {
+const loadingButtonVariants = cva(`text-white bg-indigo-700 hover:bg-indigo-800 focus:outline-none font-medium rounded text-sm px-5 py-2.5 text-center mr-2 inline-flex items-center justify-center`, {
     variants: {
         wFull : {
             true : 'w-full'
